8b790cab69f52354cd19c44acca15446a13c082d
treill_s <treill_s@epitech.eu>
2016-12-22 11:44:34 +0545

    Change folder name

---
+++ src/badwidgets.py
@@ -0,0 +1,51 @@
+from PyQt5.QtWidgets import (QWidget,
+							QHBoxLayout,
+							QFrame,
+							QSplitter,
+							QLabel)
+
+# class PlayerWidget(QWidget):
+# 	"""docstring for player"""
+# 	def __init__(self, parent):
+# 		super().__init__()
+# 		self.parent = parent
+
+# 		self.initPlayer()
+		
+# 	def initPlayer(self):
+# 		"""Set up the user interface, signals & slots
+# 		"""
+# 		hbox = QHBoxLayout(self)
+
+# 		topleft = QFrame(self)
+# 		topleft.setFrameShape(QFrame.StyledPanel)
+
+# 		topright = QFrame(self)
+# 		topright.setFrameShape(QFrame.StyledPanel)
+
+# 		bottom = QFrame(self)
+# 		bottom.setFrameShape(QFrame.StyledPanel)
+
+# 		splitter1 = QSplitter(Qt.Horizontal)
+# 		splitter1.addWidget(topleft)
+# 		splitter1.addWidget(topright)
+
+# 		splitter2 = QSplitter(Qt.Vertical)
+# 		splitter2.addWidget(splitter1)
+# 		splitter2.addWidget(bottom)
+
+# 		hbox.addWidget(splitter2)
+# 		self.setLayout(hbox)
+
+# 	def onChanged(self, text):
+
+# 		self.lbl.setText(text)
+# 		self.lbl.adjustSize() 
+
+class ExtendedQLabel(QLabel):
+
+    def __init__(self, parent):
+        QLabel.__init__(self, parent)
+
+    def mouseReleaseEvent(self, ev):
+        self.emit(SIGNAL('clicked()'))
